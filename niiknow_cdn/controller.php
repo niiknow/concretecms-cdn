d14b6c52a55551792af106ee93f09a5f1bdfd4c3
Tom Noogen <tom@tnguyen-imac.local>
2017-05-24 13:31:09 -0500

    up version

--- niiknow_cdn/controller.php
+++ niiknow_cdn/controller.php
@@ -1,8 +1,7 @@
 <?php
 namespace Concrete\Package\NiiknowCdn;
-defined('C5_EXECUTE') or die("Access Denied.");
 
-include 'lib/minifier.php';
+defined('C5_EXECUTE') or die("Access Denied.");
 
 use Concrete\Core\Package\Package;
 use Concrete\Core\Page\Page as Page;
@@ -98,6 +97,76 @@ class Controller extends Package
         $single_page = SinglePage::add('/dashboard/system/basics/niiknowcdn', $pkg);
         $single_page->update(array('cName' => t('CDN Settings'), 'cDescription' => t('CDN Settings')));
     }
+
+    // CSS Minifier => http://ideone.com/Q5USEF + improvement(s)
+    private function doCssMinify($input) {
+        if(trim($input) === "") return $input;
+        return preg_replace(
+            array(
+                // Remove comment(s)
+                '#("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\')|\/\*(?!\!)(?>.*?\*\/)|^\s*|\s*$#s',
+                // Remove unused white-space(s)
+                '#("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\'|\/\*(?>.*?\*\/))|\s*+;\s*+(})\s*+|\s*+([*$~^|]?+=|[{};,>~+]|\s*+-(?![0-9\.])|!important\b)\s*+|([[(:])\s++|\s++([])])|\s++(:)\s*+(?!(?>[^{}"\']++|"(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\')*+{)|^\s++|\s++\z|(\s)\s+#si',
+                // Replace `0(cm|em|ex|in|mm|pc|pt|px|vh|vw|%)` with `0`
+                '#(?<=[\s:])(0)(cm|em|ex|in|mm|pc|pt|px|vh|vw|%)#si',
+                // Replace `:0 0 0 0` with `:0`
+                '#:(0\s+0|0\s+0\s+0\s+0)(?=[;\}]|\!important)#i',
+                // Replace `background-position:0` with `background-position:0 0`
+                '#(background-position):0(?=[;\}])#si',
+                // Replace `0.6` with `.6`, but only when preceded by `:`, `,`, `-` or a white-space
+                '#(?<=[\s:,\-])0+\.(\d+)#s',
+                // Minify string value
+                '#(\/\*(?>.*?\*\/))|(?<!content\:)([\'"])([a-z_][a-z0-9\-_]*?)\2(?=[\s\{\}\];,])#si',
+                '#(\/\*(?>.*?\*\/))|(\burl\()([\'"])([^\s]+?)\3(\))#si',
+                // Minify HEX color code
+                '#(?<=[\s:,\-]\#)([a-f0-6]+)\1([a-f0-6]+)\2([a-f0-6]+)\3#i',
+                // Replace `(border|outline):none` with `(border|outline):0`
+                '#(?<=[\{;])(border|outline):none(?=[;\}\!])#',
+                // Remove empty selector(s)
+                '#(\/\*(?>.*?\*\/))|(^|[\{\}])(?:[^\s\{\}]+)\{\}#s'
+            ),
+            array(
+                '$1',
+                '$1$2$3$4$5$6$7',
+                '$1',
+                ':0',
+                '$1:0 0',
+                '.$1',
+                '$1$3',
+                '$1$2$4$5',
+                '$1$2$3',
+                '$1:0',
+                '$1$2'
+            ),
+        $input);
+    }
+
+    // JavaScript Minifier
+    private function doJsMinify($input) {
+        if(trim($input) === "") return $input;
+        return preg_replace(
+            array(
+                // Remove comment(s)
+                '#\s*("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\')\s*|\s*\/\*(?!\!|@cc_on)(?>[\s\S]*?\*\/)\s*|\s*(?<![\:\=])\/\/.*(?=[\n\r]|$)|^\s*|\s*$#',
+                // Remove white-space(s) outside the string and regex
+                '#("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\'|\/\*(?>.*?\*\/)|\/(?!\/)[^\n\r]*?\/(?=[\s.,;]|[gimuy]|$))|\s*([!%&*\(\)\-=+\[\]\{\}|;:,.<>?\/])\s*#s',
+                // Remove the last semicolon
+                '#;+\}#',
+                // Minify object attribute(s) except JSON attribute(s). From `{'foo':'bar'}` to `{foo:'bar'}`
+                '#([\{,])([\'])(\d+|[a-z_][a-z0-9_]*)\2(?=\:)#i',
+                // --ibid. From `foo['bar']` to `foo.bar`
+                '#([a-z0-9_\)\]])\[([\'"])([a-z_][a-z0-9_]*)\2\]#i'
+            ),
+            array(
+                '$1',
+                '$1$2',
+                '}',
+                '$1$3',
+                '$1.$3'
+            ),
+        $input);
+    }
+
     /**
      * -----------------------------------------------------------------------------------------
      * Based on `https://github.com/mecha-cms/mecha-cms/blob/master/system/kernel/converter.php`
@@ -112,22 +181,19 @@ class Controller extends Package
             return '<' . $matches[1] . preg_replace('#([^\s=]+)(\=([\'"]?)(.*?)\3)?(\s+|$)#s', ' $1$2', $matches[2]) . $matches[3] . '>';
         }, str_replace("\r", "", $input));
 
-        // Minify inline CSS declaration(s)
-        if(strpos($input, ' style=') !== false) {
-            $input = preg_replace_callback('#<([^<]+?)\s+style=([\'"])(.*?)\2(?=[\/\s>])#s', function($matches) {
-                return '<' . $matches[1] . ' style=' . $matches[2] . minify_css($matches[3]) . $matches[2];
-            }, $input);
-        }
+        // minify inline style and script tags
         if(strpos($input, '</style>') !== false) {
           $input = preg_replace_callback('#<style(.*?)>(.*?)</style>#is', function($matches) {
-            return '<style' . $matches[1] .'>'. minify_css($matches[2]) . '</style>';
+            return '<style' . $matches[1] .'>'. $this->doCssMinify($matches[2]) . '</style>';
           }, $input);
         }
+
         if(strpos($input, '</script>') !== false) {
           $input = preg_replace_callback('#<script(.*?)>(.*?)</script>#is', function($matches) {
-            return '<script' . $matches[1] .'>'. minify_js($matches[2]) . '</script>';
+            return '<script' . $matches[1] .'>'. $this->doJsMinify($matches[2]) . '</script>';
           }, $input);
         }
+
         return preg_replace(
             array(
                 // t = text
@@ -191,12 +257,12 @@ class Controller extends Package
                 if ($includeFolders) {
                     $includeFolders = str_replace('/', '\/', $includeFolders);
 
-                    $cdnUrlReg = '/\s+(href|src)\=([\"\'])\/('.$includeFolders.')/umi';
+                    $cdnUrlReg = '/\s+(href|src|srcset)\=([\"\'])\/('.$includeFolders.')/umi';
                     $contents = preg_replace($cdnUrlReg, " $1=$2__CDNURL__/$3", $contents);
 
                     // 2-2. regex for inline url
-                    $inlineReg = '(\s*url\s*\(\s*[\'\"]?\/)('.$includeFolders.')+([^)]+)/umi';
-                    $contents = preg_replace($cdnUrlReg, "$1__CDNURL__$3", $contents);
+                    $inlineReg = '/(\s*url\s*\(\s*[\'\"]?)(\/)('.$includeFolders.')+([^\)]+)/umi';
+                    $contents = preg_replace($inlineReg, "$1__CDNURL__$2$3$4", $contents);
 
                     // 3. replace excluded back to just relative URL
                     $excludeSubstrings = $that->getExcludeSubstrings();
